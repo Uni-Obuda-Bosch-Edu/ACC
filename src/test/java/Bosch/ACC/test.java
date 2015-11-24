54a3cc3eaff15f802b08700f1bf2c543ff6e0633
Mike <Mike@192.168.1.66>
2015-11-24 21:18:36 +0100

    Néhány extra teszt

--- src/test/java/Bosch/ACC/test.java
+++ src/test/java/Bosch/ACC/test.java
@@ -18,6 +18,8 @@ public class test {
 		
 		DummyAccSetGet dummySharedMemory = new DummyAccSetGet();
 		
+		//Nothing in front
+		System.out.println("Nothing in front");
 		dummySharedMemory.releaseBrakePedal(100);
 		dummySharedMemory.releaseGasPedal(100);
 		dummySharedMemory.DistanceOfClosestCarRadar = -1;
@@ -36,6 +38,45 @@ public class test {
 		
 		dummySharedMemory.Speed = 21;
 		printIterate(1, acc, dummySharedMemory);
+		
+		//Car in front - increasing distance
+		System.out.println("Car in front - increasing distance");
+		dummySharedMemory.Speed = 15;
+		dummySharedMemory.DistanceOfClosestCarRadar = 20;
+		printIterate(1, acc, dummySharedMemory);
+		
+		dummySharedMemory.DistanceOfClosestCarRadar = 21;
+		printIterate(1, acc, dummySharedMemory);
+		
+		dummySharedMemory.DistanceOfClosestCarRadar = 25;
+		printIterate(1, acc, dummySharedMemory);
+		
+		//Car in front - same speed (distance = 25)
+		System.out.println("Car in front - same speed (distance = 25)");
+		printIterate(1, acc, dummySharedMemory);
+		printIterate(1, acc, dummySharedMemory);
+		printIterate(1, acc, dummySharedMemory);
+		
+		//Car closing in distance
+		System.out.println("Car closing in distance");
+		dummySharedMemory.DistanceOfClosestCarRadar = 24;
+		printIterate(1, acc, dummySharedMemory);
+		
+		dummySharedMemory.DistanceOfClosestCarRadar = 22;
+		printIterate(1, acc, dummySharedMemory);
+		
+		dummySharedMemory.DistanceOfClosestCarRadar = 19;
+		printIterate(1, acc, dummySharedMemory);
+		
+		//Curve
+		System.out.println("Curve = 45°");
+		dummySharedMemory.Curve = 45;
+		dummySharedMemory.Speed = 90;
+		printIterate(1, acc, dummySharedMemory);
+		printIterate(1, acc, dummySharedMemory);
+		printIterate(1, acc, dummySharedMemory);
+		printIterate(1, acc, dummySharedMemory);
+
 	}
 
 }
